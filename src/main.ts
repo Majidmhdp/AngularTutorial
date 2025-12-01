312329e7bbba11acd22adfe44a884343e18b2165
Majid.mhdp <Majid.mhdp@gmail.com>
2025-12-01 18:23:05 +0800

    add component

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { Header } from './app/header';
 
 bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
-
-bootstrapApplication(Header, appConfig)
   .catch((err) => console.error(err));
