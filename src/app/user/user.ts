f5c498596fb169030e3bdf244e22c1d3a378d2a1
Majid.mhdp <Majid.mhdp@gmail.com>
2025-12-02 10:31:27 +0800

    component output

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,5 +1,5 @@
-import { Component, Input, input, signal, computed } from '@angular/core';
-import { required } from '@angular/forms/signals';
+import { Component, Input, input, signal, computed, Output, EventEmitter } from '@angular/core';
+// import { required } from '@angular/forms/signals';
 // import { DUMMY_USERS } from '../dummy-users';
 
 // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -11,24 +11,26 @@ import { required } from '@angular/forms/signals';
   styleUrl: './user.css',
 })
 export class User {
+  @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name: string = '';
   // name = input.required<string>();
 
-  
+  @Output() select = new EventEmitter();
+
   // selectedUser = signal(DUMMY_USERS[randomIndex]);
   // getPath = computed(() => 'images/users/' + this.selectedUser().avatar);
 
   get getPath() {
     // return 'images/users/' + this.selectedUser.avatar
-    return 'images/users/' + this.avatar
+    return 'images/users/' + this.avatar;
   }
-  
 
-  // onClickUser() {
-  //   const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+  onClickUser() {
+    //   const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
-  //   this.selectedUser.set(DUMMY_USERS[randomIndex]);
-  //   // this.selectedUser = DUMMY_USERS[randomIndex];
-  // }
+    //   this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    //   // this.selectedUser = DUMMY_USERS[randomIndex];
+    this.select.emit(this.id);
+  }
 }
